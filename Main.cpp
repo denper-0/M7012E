c79508532e5b634cd3380be4f80f34fff5a7e068
raxo <oskar.klang@gmail.com>
2013-11-18 17:03:26 +0100

    project files with main function

---
+++ Main.cpp
@@ -0,0 +1,19 @@
+#include "Main.h"
+#include <iostream>
+using namespace std;
+
+Main::Main(void)
+{
+
+}
+
+
+int main(void)
+{
+
+	return 0;
+}
+
+Main::~Main(void)
+{
+}
---
+++ Main.h
@@ -0,0 +1,12 @@
+#pragma once
+
+
+#include "Leap.h"
+using namespace Leap;
+
+class Main
+{
+public:
+	Main(void);
+	~Main(void);
+};

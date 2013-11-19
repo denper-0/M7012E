71dae0f84f09d2fbf8af204351b23668f927de85
raxo <oskar.klang@gmail.com>
2013-11-19 10:35:20 +0100

    leap file

--- Main.cpp
+++ Main.cpp
@@ -10,7 +10,12 @@ Main::Main(void)
 
 int main(void)
 {
-
+	Controller controller;
+	if( controller.isConnected()) //controller is a Controller object
+        {
+                Frame frame = controller.frame(); //The latest frame
+                Frame previous = controller.frame(1); //The previous frame
+        }
 	return 0;
 }
 

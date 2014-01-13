285e86027218411e366aef2521d6ad81f3c20407
JamEijsden <Katat0nia>
2014-01-13 16:29:13 +0100

    ADDED COMMENTS
    roomstate and room

--- Room.cpp
+++ Room.cpp
@@ -11,16 +11,37 @@ Room::~Room(void) {
 	//?? memoryleeekekekes?
 }
 
+/**
+ @breif returns a room as a door in a roomstate at a certain direction
+ @param int dir is the direction facing in the room 
+ @param string desc the description
+ */
 void Room::setDescription(int rd, std::string desc){
 	currentRoomState->setDesc(rd, desc);
 }
+
+/**
+ @breif returns a description
+ @param int dir is the direction facing in the room 
+ @return 
+ */
 std::string Room::getDescription(int rd) {
 	return currentRoomState->getDesc(rd);
 }
 
+/**
+ @breif add event to the event vector
+ @param Event* e the event to be added 
+ */
 void Room::setEvent(Event* e) {
 	events.push_back(e);
 }
+
+/**
+ @breif returns event on action
+ @param int action an action that occured
+ @return vector<Event*> returns vector with event connected to action
+ */
 std::vector<Event*> Room::getEvents(int action) {
 	std::vector<Event*> ret;
 	for(size_t i =0; i < events.size(); i++) {
@@ -31,6 +52,10 @@ std::vector<Event*> Room::getEvents(int action) {
 	return ret;
 }
 
+/**
+ @breif returns what action the room's event's react too 
+ @return vector<MyLeapAction> vector with allowed action for the event in a room
+ */
 std::vector<MyLeapAction> Room::getAllowedActions() {
 	std::vector<MyLeapAction> ret;
 	for(size_t i =0; i<events.size(); i++) {
@@ -39,22 +64,46 @@ std::vector<MyLeapAction> Room::getAllowedActions() {
 	return ret;
 }
 
+/**
+ @breif adds door
+ @param int rd direction in the room
+ @param Room* r the room that the door is going to lead to
+ */
 void Room::setDoor(int rd, Room* r) {
 	currentRoomState->setDoor(rd, r);
 }
 
+/**
+ @breif returns the room the door is connected to
+ @param int dir is the direction facing in the room 
+ @return Room* room the door is connected too 
+ */
 Room* Room::getDoor(int rd) {
 	return currentRoomState->getDoor(rd);
 }
+
+/**
+ @breif adds item to room
+ @param int dir is the direction facing in the room 
+ */
 
 void Room::setItem(int i, int itemId) {
 	currentRoomState->setItem(i, itemId);
 }
 
+/**
+ @breif returns the current state for the room
+ @return State* current stat for the room
+ */
 State* Room::getState() {
 	return currentRoomState;
 }
 
+/**
+ @breif overwrites this room's state with given roomstate, NULL IGNORES
+ @param State* s roomstate to overwrite with
+ @param State* s2 the Playerstate to know facing 
+ */
 void Room::overWrite(State* s, State* s2) {
 	RoomState* rs = static_cast<RoomState*>(s); // typecast
 	PlayerState* playerState = static_cast<PlayerState*>(s2); // typecast
@@ -82,6 +131,12 @@ void Room::overWrite(State* s, State* s2) {
 	}
 }
 
+/**
+ @breif checks if this room's state is equal to the given roomstate
+ @param State* s roomstate to compare with
+ @param State* s2 the Playerstate to know facing 
+ @return boolean if equal true ELSE false
+ */
 bool Room::isEqual(State* s, State* s2) {
 	RoomState* rs = static_cast<RoomState*>(s); // typecast
 	PlayerState* currentPlayer = static_cast<PlayerState*>(s2); // typecast
@@ -114,11 +169,19 @@ bool Room::isEqual(State* s, State* s2) {
 	return true;
 }
 
+/**
+ @breif returns roomId
+ @return int the rooms id 
+ */
 int Room::getRoomId() {
 	return roomId;
 }
   
-
+/**
+ @breif overwrites this room's state with given roomstate, NULL IGNORES
+ @param State* s roomstate to overwrite with
+ @param State* s2 the Playerstate to know facing 
+ */
 int Room::getRoomIsLocked() {
 	return currentRoomState->getRoomIsLocked();
 }
--- RoomState.cpp
+++ RoomState.cpp
@@ -11,7 +11,11 @@ RoomState::RoomState(void)
 RoomState::~RoomState(void)
 {
 }
-
+/**
+ @breif sets roomdescription on the roomstate
+ @param int rd This is the facing/direction in the room 
+ @param string desc The description text
+ */
 void RoomState::setDesc(int rd, std::string desc) {
 	if(description.size() <= rd) {
 		description.resize(rd+1, "");
@@ -19,6 +23,11 @@ void RoomState::setDesc(int rd, std::string desc) {
 	description.at(rd) = desc;
 }
 
+/**
+ @breif Returns room description from the roomstate
+ @param int rd This is the facing/direction in the room 
+ @return string The text description of a certain facing
+ */
 std::string RoomState::getDesc(int rd){
 	if(description.size() <= rd) {
 		return "";
@@ -27,6 +36,12 @@ std::string RoomState::getDesc(int rd){
 
 }
 
+/**
+ @breif Sets a room as a door in a roomstate at a certain direction
+ @param int dir is the direction facing in the room 
+ @param the room that is added a door too
+ 
+ */
 void RoomState::setDoor(int dir, Room* door) {
 	if(doors.size() <= dir) {
 		doors.push_back(door);
@@ -35,6 +50,11 @@ void RoomState::setDoor(int dir, Room* door) {
 	this->doors[dir] = door;
 }
 
+/**
+ @breif returns a room as a door in a roomstate at a certain direction
+ @param int dir is the direction facing in the room 
+ @return Room* ELSE NULL Door to next room 
+ */
 Room* RoomState::getDoor(int dir){
 	if(doors.size() <= dir) {
 		return NULL;
@@ -42,10 +62,19 @@ Room* RoomState::getDoor(int dir){
 	return this->doors[dir];
 }
 
-
+/**
+ @breif Checks if room is locked
+ @return int a int with value if locked or not
+ */
 int RoomState::getRoomIsLocked() {
 	return roomIsLocked;
 }
+
+/**
+ @breif locks a room
+ @param int locked is the lock value 
+ */
+
 void RoomState::setRoomIsLocked(int locked) {
 	roomIsLocked = locked;
 }

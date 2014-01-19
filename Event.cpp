4b6d0f8d662375dbc3a0f648a1553e4700155726
raxo <oskar.klang@gmail.com>
2014-01-19 14:27:29 +0100

    Update event.c with comments

--- Event.cpp
+++ Event.cpp
@@ -1,6 +1,15 @@
 #include "Event.h"
 
 
+/**
+ @breif constructs a event with some properties set, rest are set to deafult meaning no change if it fires
+ @param MyLeapAction* on what action the event fires
+ @param PlayerState* current player state the game loop overwrites with
+ @param RoomState* current room state the game loop verifies with
+ @param PlayerState* new room state the game loop overwrites with
+ @param RoomState* new room state the game loop overwrites with
+ @param string eventStr text for the event
+ */
 Event::Event(MyLeapAction a, PlayerState* current_PS, RoomState* current_RS, PlayerState* newPS, RoomState* newRS, std::string eText) {
 	action = a;
 	cur_PS = current_PS;
@@ -13,11 +22,14 @@ Event::Event(MyLeapAction a, PlayerState* current_PS, RoomState* current_RS, Pla
 	Room* barelyEscapableRoom = NULL;
 }
 
+/**
+ * @breif destructor
+ */
 Event::~Event(void) {
 
 }
 /**
- @breif set text/desciptrition on event
+ @breif set text/desciptrition on event that will output when event is done
  @param string eventStr text for the event
  */
 void Event::setText(std::string eventStr){
@@ -32,7 +44,7 @@ std::string Event::getText(){
 	 return eventText;
 }
 /**
- @breif sets on what playerstate event skould happend
+ @breif sets on what playerstate event should happend and what it will overwrite with
  @param Playerstate* currentPS the current player state
  @param Playerstate* newPS the new playerstate
  */
@@ -42,7 +54,7 @@ void Event::setPlayerState(PlayerState* currentPS, PlayerState* newPS){
 }
 
 /**
- @breif sets on what roomstate event skould happend(current) and how that state look afterwards
+ @breif sets on what roomstate event should happend(current) and how that state look afterwards
  @param Roomstate* currentRS the current player state
  @param Roomstate* newRS the new playerstate
  */
@@ -86,28 +98,29 @@ MyLeapAction Event::getAction() {
 	return action;
 }
 /**
- @breif if you are able to move to next room
+ @breif if the event will the player to next room
  @return bool 
  */
 bool Event::getMoveToNextRoom() {
 	return moveToNextRoom;
 }
 /**
- @breif set if can move to next room
+ @breif set if will move to next room
  @param bool isOnThisEvent 
  */
 void Event::setMoveToNextRoom(bool isOnThisEvent) {
 	moveToNextRoom = isOnThisEvent;
 }
 /**
- @breif returns true if barelyEscapable ELSE false
- @return bool
+ @breif if this event moves to a barelyEscapable room
+ @return bool true if barelyEscapable ELSE false
  */
 bool Event::isBarelyEscapable() {
 	return barelyEscapable;
 }
 /**
  @breif sets if the event is an barelyEscapable(like the Inventory)
+ @param Room* pointer to the barelyEscapable room
  */
 void Event::setBarelyEscapable(Room *barelyEscapableRoom) {
 	this->barelyEscapableRoom = barelyEscapableRoom;
@@ -120,22 +133,22 @@ void Event::setBarelyEscapable(Room *barelyEscapableRoom) {
 
 /**
  @breif returns barelyEscapableRoom variable
- @return Room*
+ @return Room*  pointer to the barelyEscapable room
  */
 Room* Event::getBarelyEscapableRoom() {
 	return barelyEscapableRoom;
 }
 /**
- @breif adds that the event triggers on an action
- @param OnEventAction action is action to be added to event
+ @breif adds a action the event will trigger after it is fired
+ @param OnEventAction is action to be added to event
  */
 void Event::setActionOnEvent(OnEventAction action) {
 	this->eventActions.push_back(action);
 }
 
 /**
- @breif returns the vector of actions the event triggers on
- @return vector<OnEventAction>
+ @breif  gets actions the event will triggers
+ @return vector<OnEventAction> the vector of actions the event will triggers
  */
 std::vector<OnEventAction> Event::getActionOnEvent() {
 	return this->eventActions;
